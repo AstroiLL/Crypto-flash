552a37a45aed848725a2aecbe3ba1d31aaf467a7
astroill <astroill@gmail.com>
2020-10-05 20:09:51 +0300

    Первый коммит

--- DiLL/crypto.py
+++ DiLL/crypto.py
@@ -1,1 +1,206 @@
-/home/astroill/Python/DiLL/crypto.py
+"""
+Binance functions
+"""
+import time
+from datetime import datetime, timedelta
+
+import ccxt
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Констатнты
+D1 = 86400000  # ms
+H1 = 3600000  # ms
+M1 = 60000  # ms
+M5 = M1 * 5
+# H1s = 3600  # s
+verbose = False
+mysql_btc = 'mysql://bitok:bitok@10.10.10.200:3307'
+
+class Crypto:
+    def __init__(self, exchange='BITMEX', crypto='BTC/USD', period='1d', indexes=True, tz=3):
+        if verbose: print(f'==============\nInit {exchange}.{crypto}')
+        try:
+            conn = create_engine(f'{mysql_btc}').connect()
+        except:
+            print('Error open MySQL')
+            exit(1)
+        self.exchange = exchange
+        if self.exchange != 'BITMEX' and self.exchange != 'BINANCE':
+            print(f'Incorrect exchange {self.exchange}')
+            exit(2)
+        self.crypto = crypto
+        self.dict_period = {'1m': M1, '1h': H1, '1d': D1}
+        self.period = period
+        self.limit = None
+        self.last_date = 0
+        self.indexes = indexes
+        self.tz = tz
+        self.df = pd.DataFrame()
+        try:
+            self.conn = create_engine(f'{mysql_btc}/{self.exchange}.{self.crypto}').connect()
+        except:
+            print(f'No base {self.exchange}.{self.crypto}')
+            self._create_base()
+        result = self.get_count_records()
+        if result == 0:
+            print(f'Empty base {self.exchange}.{self.crypto}')
+            self._get_crypto_from_exchange()
+            result = self.get_count_records()
+            if result == 0:
+                print(f'Error filling base {self.exchange}.{self.crypto}')
+                # self.delete({self.exchange}.{self.crypto})
+                exit(3)
+        print(f"Table {self.period} has total {result} records")
+        self.get_list_exch()
+
+    def get_count_records(self):
+        return self.conn.execute(f"SELECT COUNT(*) FROM {self.period}").fetchone()[0]
+
+    def get_fist_date(self):
+        df = pd.read_sql(f"SELECT * FROM {self.period} ORDER BY Date LIMIT 1", con=self.conn)
+        if verbose: print(f"Fist date {df.at[0, 'Date']}")
+        return df.at[0, 'Date']
+
+    def get_last_date(self):
+        result = self.get_count_records()
+        print(f"Table {self.period} has total {result} records")
+        if result == 0:
+            print("Empty base")
+            self._get_crypto_from_exchange()
+        df = pd.read_sql(f"SELECT * FROM {self.period} ORDER BY Date DESC LIMIT 1", con=self.conn)
+        return df.at[0, 'Date']
+
+    def load_crypto(self, limit=None):
+        if verbose:
+            result = self.get_count_records()
+            print(f"Table {self.period} has total {result} records")
+            print(f"Load {self.crypto} from SQL {self.period}")
+        if limit is not None: self.limit = limit
+        if self.limit is None:
+            self.df = pd.read_sql(f"SELECT * FROM {self.period} ORDER BY Date DESC", con=self.conn)
+            self.limit = self.df.shape[0]
+        else:
+            self.df = pd.read_sql(f"SELECT * FROM {self.period} ORDER BY Date DESC LIMIT {self.limit}", con=self.conn)
+            self.limit = min(self.df.shape[0], self.limit)
+        self.last_date = self.df.at[0, 'Date']
+        if verbose: print("Last date from mySQL", self.last_date)
+        self.df.sort_values('Date', ascending=True, inplace=True)
+        self.df['Date'] += timedelta(hours=self.tz)
+        if self.indexes:
+            self.df.set_index('Date', drop=True, inplace=True)
+        print(f'Loaded {self.limit} bars {self.period}')
+        return self.df
+
+    def update_crypto(self):
+        if verbose: print(f"Update {self.crypto} from {self.exchange} {self.period}")
+        self.last_date = self.get_last_date()
+        if self.last_date == 0:
+            self._get_crypto_from_exchange()
+            self.last_date = self.get_last_date()
+        if verbose: print("Last date from mySQL", self.last_date)
+        today = datetime.utcnow()
+        if verbose: print('Today:', today)
+        difs_td = today - self.last_date
+        if verbose: print('Today - last_date =', difs_td)
+        # difs = 0
+        if self.period == '1d':
+            difs = difs_td.days
+        elif self.period == '1h':
+            difs = int(difs_td.total_seconds() // 3600)
+        elif self.period == '1m':
+            difs = int(difs_td.total_seconds() // 60)
+        else:
+            return None
+        # if verbose: print(difs)
+        if difs >= 1:
+            self.conn.execute(f"DELETE FROM {self.period} ORDER BY Date DESC LIMIT 1", con=self.conn)
+            self._get_crypto_from_exchange(limit=difs + 1)
+
+    def _crypto_to_sql(self, df_app: pd.DataFrame):
+        df_app.set_index('Date', drop=True, inplace=True)
+        try:
+            df_app.to_sql(self.period, con=self.conn, if_exists='append', index=True)
+        except:
+            print('Error write to MySQL')
+            pass
+
+    def _get_crypto_from_exchange(self, since=None, limit=500):
+        print(f"Get {self.crypto} from {self.exchange} {self.period}")
+        if self.exchange == 'BITMEX':
+            exchange = ccxt.bitmex()
+        elif self.exchange == 'BINANCE':
+            exchange = ccxt.binance()
+        else:
+            print(f'Incorrect exchange {self.exchange}')
+            exit(3)
+        if since is None:
+            since = exchange.milliseconds() - self.dict_period[self.period] * limit
+        else:
+            since = exchange.parse8601(since + 'T00:00:00Z')
+        # print(exchange.iso8601(since))
+        while limit > 0:
+            if verbose: print('Load limit', limit, self.period)
+            lmt = limit if limit <= 750 else 750
+            try:
+                fetch = exchange.fetch_ohlcv(self.crypto, self.period, since=since, limit=lmt)
+            except:
+                print(f'Error fetch from {self.exchange}')
+                exit(1)
+            else:
+                df = pd.DataFrame(fetch, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
+                df['Date'] = pd.to_datetime(df['Date'], unit='ms', infer_datetime_format=True)
+                self._crypto_to_sql(df)
+            since += self.dict_period[self.period] * lmt
+            limit -= lmt
+            if limit > 0: time.sleep(exchange.rateLimit // 100)
+
+    def _create_base(self):
+        base1 = f"CREATE DATABASE IF NOT EXISTS `{self.exchange}.{self.crypto}`; USE `{self.exchange}.{self.crypto}`;"
+        base2 = """
+CREATE TABLE `1d` (
+  `Date` datetime NOT NULL,
+  `Open` double DEFAULT NULL,
+  `High` double DEFAULT NULL,
+  `Low` double DEFAULT NULL,
+  `Close` double DEFAULT NULL,
+  `Volume` double DEFAULT NULL
+);
+CREATE TABLE `1h` (
+  `Date` datetime NOT NULL,
+  `Open` double DEFAULT NULL,
+  `High` double DEFAULT NULL,
+  `Low` double DEFAULT NULL,
+  `Close` double DEFAULT NULL,
+  `Volume` double DEFAULT NULL
+);
+CREATE TABLE `1m` (
+  `Date` datetime NOT NULL,
+  `Open` double DEFAULT NULL,
+  `High` double DEFAULT NULL,
+  `Low` double DEFAULT NULL,
+  `Close` double DEFAULT NULL,
+  `Volume` double DEFAULT NULL
+);
+ALTER TABLE `1d`
+  ADD PRIMARY KEY (`Date`) USING BTREE;
+ALTER TABLE `1h`
+  ADD PRIMARY KEY (`Date`) USING BTREE;
+ALTER TABLE `1m`
+  ADD PRIMARY KEY (`Date`) USING BTREE;
+COMMIT;
+        """
+        if verbose: print(f'Created base {self.exchange}.{self.crypto}')
+        conn = create_engine(f'{mysql_btc}/').connect()
+        conn.execute(base1 + base2, con=conn)
+        conn = create_engine(f'{mysql_btc}/EXCHANGE').connect()
+        ins = f"INSERT INTO `Exchange`(`Exchange`, `Crypto`) VALUES ('{self.exchange}','{self.crypto}')"
+        conn.execute(ins, con=conn)
+        self.conn = create_engine(f'{mysql_btc}/{self.exchange}.{self.crypto}').connect()
+
+    def get_list_exch(self):
+        conn = create_engine(f'{mysql_btc}/EXCHANGE').connect()
+        df = pd.read_sql(f"SELECT * FROM Exchange", con=conn)
+        if verbose: print(df)
+        return df
+
--- DiLL/utils.py
+++ DiLL/utils.py
@@ -1,1 +1,324 @@
-/home/astroill/Python/DiLL/utils.py
+"""
+iLLs functions
+"""
+import os
+import os.path
+
+# import ccxt
+import joblib  # for persistence
+import matplotlib.dates as mdates
+import pandas as pd
+from matplotlib.lines import Line2D
+from matplotlib.patches import Rectangle
+
+# from sqlalchemy import create_engine
+
+# Констатнты
+D1 = 86400000  # ms
+H1 = 3600000  # ms
+M1 = 60000  # ms
+M5 = M1 * 5
+
+
+# H1s = 3600  # s
+
+def ep(*args):
+    '''
+    print and exit
+    '''
+    print(args)
+    exit()
+
+
+def SMA(x: pd.Series, period: int = 20) -> pd.Series:
+    """
+    Скользящая средняя
+    :param x: Series
+    :param period: Период sma
+    :return: Series
+    """
+    return (x.rolling(period).mean())
+
+
+def hd(x, precision=2):
+    """
+    Округляет большие числа и преобразует их в строку в виде удобном для восприятия человеком.
+    Добавляя после числа
+    G для миллиардов
+    M для миллионов
+    k для тысяч
+    :param x: число для преобразования
+    :param precision: число знаков после точки (пример: для 2 будет 3.86M, по умолчанию 2)
+    :return: строку вида "3.86M"
+    """
+    if precision < 0: return (str(x))
+    y = 0.0
+    suf = ''
+    pref = ''
+    x = float(x)
+    if x < 0:
+        x = abs(x)
+        pref = '-'
+    if x >= 1e9:
+        y = x / 1e9
+        y = round(y, precision)
+        suf = 'G'
+    elif x >= 1e6:
+        y = x / 1e6
+        y = round(y, precision)
+        suf = 'M'
+    elif x >= 1000:
+        y = x / 1000
+        y = round(y, precision)
+        suf = 'k'
+    return pref + f'{y:.{precision}f}' + suf
+
+
+def lev2font(x, max_v, lev_m, max_f, min_f):
+    # if not isinstance(x,type(float)):
+    #     return min_f
+    # ep(x, max_v, lev_m, max_f, min_f)
+    x = float(x)
+    # print(x)
+    font = int((((max_f - min_f) * (x - max_v * lev_m)) / (max_v * (1 - lev_m))) + min_f)
+    return (font if font >= 0 else 0)
+
+
+def wa(group):
+    group['Price'] = group['Price'] * group['Size'] / group['Size'].sum()
+    # group['fairePrice'] = group['fairePrice'] * group['Size'] / group['Size'].sum()
+    return group
+
+
+def ohlc2(axes, prices: pd.DataFrame, colorup='blue', colordown='red'):
+    width = 0.0001
+    width2 = 0.0004
+    pricesup = prices[prices['Close'] >= prices['Open']]
+    pricesdown = prices[prices['Close'] < prices['Open']]
+
+    axes.bar(pricesup.index, pricesup['Close'] - pricesup['Open'], width, bottom=pricesup['Open'], color=colorup)
+    # axes.bar(pricesup.index, pricesup['High'] - pricesup['Close'], width2, bottom=pricesup['Close'], color=colorup)
+    # axes.bar(pricesup.index, pricesup['Low'] - pricesup['Open'], width2, bottom=pricesup['Open'], color=colorup)
+
+    axes.bar(pricesdown.index, pricesdown['Close'] - pricesdown['Open'], width, bottom=pricesdown['Open'],
+             color=colordown)
+    # axes.bar(pricesdown.index, pricesdown['High'] - pricesdown['Open'], width2, bottom=pricesdown['Open'], color=colordown)
+    # axes.bar(pricesdown.index, pricesdown['Low'] - pricesdown['Close'], width2, bottom=pricesdown['Close'], color=colordown)
+
+
+def ohlc_j(ax, quotes, width=0.2, colorup='blue', colordown='red',
+           alpha=1.0):
+    """
+    Plot the time, open, high, low, close as a vertical line ranging
+    from low to high.  Use a rectangular bar to represent the
+    open-close span.  If close >= open, use colorup to color the bar,
+    otherwise use colordown
+
+    Parameters
+    ----------
+    ax : `Axes`
+        an Axes instance to plot to
+    quotes : sequence of quote sequences
+        data to plot.  time must be in float date format - see date2num
+        (time, open, high, low, close, ...)
+    width : float
+        fraction of a day for the rectangle width
+    colorup : color
+        the color of the rectangle where close >= open
+    colordown : color
+         the color of the rectangle where close <  open
+    alpha : float
+        the rectangle alpha level
+
+    Returns
+    -------
+    ret : tuple
+        returns (lines, patches) where lines is a list of lines
+        added and patches is a list of the rectangle patches added
+
+    """
+
+    OFFSET = width / 2.0
+    lines = []
+    patches = []
+    for q in quotes:
+        t, open, high, low, close = q[:5]
+        td = mdates.date2num(t.to_pydatetime())
+
+        if close >= open:
+            color = colorup
+            lower = open
+            height = close - open
+        else:
+            color = colordown
+            lower = close
+            height = open - close
+
+        vline = Line2D(
+            xdata=(t, t), ydata=(low, high),
+            color=color,
+            linewidth=0.5,
+            antialiased=True,
+        )
+
+        rect = Rectangle(
+            # xy=(t, lower),
+            xy=(td - OFFSET, lower),
+            width=width,
+            height=height,
+            facecolor=color,
+            edgecolor=color,
+        )
+        rect.set_alpha(alpha)
+
+        lines.append(vline)
+        patches.append(rect)
+        ax.add_line(vline)
+        ax.add_patch(rect)
+    ax.autoscale_view()
+
+    return lines, patches
+
+
+def ohlc_ha(ax, quotes, width=0.2, colorup='blue', colordown='red',
+            alpha=1.0):
+    """
+    Plot the time, open, high, low, close as a vertical line ranging
+    from low to high.  Use a rectangular bar to represent the
+    open-close span.  If close >= open, use colorup to color the bar,
+    otherwise use colordown
+
+    Parameters
+    ----------
+    ax : `Axes`
+        an Axes instance to plot to
+    quotes : sequence of quote sequences
+        data to plot.  time must be in float date format - see date2num
+        (time, open, high, low, close, ...)
+    width : float
+        fraction of a day for the rectangle width
+    colorup : color
+        the color of the rectangle where close >= open
+    colordown : color
+         the color of the rectangle where close <  open
+    alpha : float
+        the rectangle alpha level
+
+    Returns
+    -------
+    ret : tuple
+        returns (lines, patches) where lines is a list of lines
+        added and patches is a list of the rectangle patches added
+
+    """
+
+    OFFSET = width / 2.0
+    lines = []
+    patches = []
+    open_p = quotes[0][1]
+    close_p = quotes[0][4]
+    # ep(open_p)
+    for q in quotes:
+        t, open, high, low, close = q[:5]
+        td = mdates.date2num(t.to_pydatetime())
+
+        closeh = (open + close + high + low) / 4
+        openh = (open_p + close_p) / 2
+        open_p = openh
+        close_p = closeh
+        highh = max(high, openh, closeh)
+        lowh = min(low, openh, closeh)
+        open = openh
+        close = closeh
+        high = highh
+        low = lowh
+
+        if close >= open:
+            color = colorup
+            lower = open
+            height = close - open
+        else:
+            color = colordown
+            lower = close
+            height = open - close
+
+        vline = Line2D(
+            xdata=(t, t), ydata=(low, high),
+            color=color,
+            linewidth=0.5,
+            antialiased=True,
+        )
+
+        rect = Rectangle(
+            # xy=(t, lower),
+            xy=(td - OFFSET, lower),
+            width=width,
+            height=height,
+            facecolor=color,
+            edgecolor=color,
+        )
+        rect.set_alpha(alpha)
+
+        lines.append(vline)
+        patches.append(rect)
+        ax.add_line(vline)
+        ax.add_patch(rect)
+    ax.autoscale_view()
+
+    return lines, patches
+
+
+# #############################################################################
+# Persist a model and create predictions after re-loading it
+def check_pkl(name="arima.pkl"):
+    return os.path.isfile(name)
+
+
+def del_pkl(name="arima.pkl"):
+    os.unlink(name)
+
+
+def save_pkl(arima, name="arima.pkl"):
+    # Pickle it
+    joblib.dump(arima, name, compress=3)
+    print(f'Saved {name}')
+
+
+def load_pkl(name="arima.pkl"):
+    # Load the model up, create predictions
+    arima = joblib.load(name)
+    print(f'Loaded {name}')
+    return arima
+
+
+"""
+class Disk():
+    def check_(self):
+        return os.path.isfile(self.name)
+
+    def load(self):
+        if check_():
+            arima = joblib.load(self.name)
+            self.loaded = True
+            print(f'Loaded {self.name}')
+        return arima
+
+    def __init__(self, name="arima.pkl", new=True):
+        self.name = name
+        self.var = None
+        self.loaded = False
+        if not new:
+            if check_():
+                self.var = load()
+                self.loaded = True
+
+    def delete(self):
+        if check_():
+            os.unlink(self.name)
+            print(f'Deleted {self.name}')
+
+    def save(self, var):
+        if self.loaded:
+            joblib.dump(var, self.name, compress=3)
+            print(f'Saved {self.name}')
+"""
