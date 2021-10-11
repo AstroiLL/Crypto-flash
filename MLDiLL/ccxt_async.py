ff40c06352c12ac11593f284a8d3b4f662d6cc78
astroill <astroill@gmail.com>
2021-10-11 23:14:53 +0300

    ccx async test

---
+++ MLDiLL/ccxt_async.py
@@ -0,0 +1,8 @@
+import asyncio
+import ccxt.async_support as ccxt
+
+async def print_poloniex_ethbtc_ticker():
+    poloniex = ccxt.poloniex()
+    print(await poloniex.fetch_ticker('ETH/BTC'))
+
+asyncio.get_event_loop().run_until_complete(print_poloniex_ethbtc_ticker())
