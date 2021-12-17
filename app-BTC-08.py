03c601a85834a961486b1d18dd71a7a84ae74350
astroill <astroill@gmail.com>
2021-12-17 23:59:43 +0300

    app-BTC-08.py Refresh update

--- app-BTC-08.py
+++ app-BTC-08.py
@@ -17,7 +17,7 @@ from MLDiLL.utils import hd, wvwma, sma
 PERIOD = '1m'
 LIMIT = 800
 WVW = 24
-VERSION = 'BTC Splash #07'
+VERSION = 'BTC Splash #08'
 cry = CryptoA(period=PERIOD, verbose=False)
 cry.load(limit=LIMIT)
 
@@ -84,12 +84,12 @@ sma_selector = html.Div(
 vol_level_selector = dcc.RangeSlider(
     id='vol-level-slider',
     min=0,
-    max=cry.maxV,
-    value=[cry.maxV * 0.25, cry.maxV * 0.e55],
+    max=100,
+    value=[25, 50],
     # allowCross=False,
     # pushable=1000000,
     tooltip={'always_visible': True, 'placement': 'bottom'},
-    # persistence=True, persistence_type='local',
+    persistence=True, persistence_type='local',
 )
 
 price_line = html.Div(
@@ -155,9 +155,10 @@ app.layout = html.Div(
         ),
         dbc.Row(
             [
-                dbc.Col(dbc.Button('Refresh', id="refresh", color="primary", outline=True), width=1),
-                dbc.Col(dcc.Loading(html.Div(id='out-btc'))),
-                dbc.Col(html.Div(id='out-dump'), width=0),
+                dbc.Col(dcc.Loading(dbc.Button('Refresh', id="refresh", color="primary", outline=True)), width=1),
+                dbc.Col(html.Div(id='out-btc')),
+                # dbc.Col(dbc.Button('-', id="refresh", color="primary", outline=True), width=1),
+                # dbc.Col(dcc.Loading(html.Div(id='out-btc'))),
                 dbc.Col(price_line, width=0),
                 dbc.Col(sma_period, width=0),
                 dbc.Col(price_sma, width=0),
@@ -180,42 +181,40 @@ app.layout = html.Div(
 
 
 @app.callback(
-    [Output('out-dump', 'children'),
-     Output('vol-level-slider', 'max'),
-     ],
-    [Input('refresh', 'n_clicks'),
-     Input('interval-reload', 'n_intervals'),
-     ]
+    Output('refresh', 'children'),
+    Input('refresh', 'n_clicks'),
+    Input('interval-reload', 'n_intervals'),
 )
 def update_df(n, nn):
     cry.load(limit=LIMIT)
-    return ' ', cry.maxV
+    return 'Refresh'
+    # , cry.maxV
     # , [cry.maxV*0.5, cry.maxV*0.75]
 
 
 @app.callback(
-    [Output('btc-chart', 'children'),
-     Output('out-btc', 'children')],
-    [Input('refresh', 'n_clicks'),
-     Input('vol-level-slider', 'value'),
-     Input('interval-reload', 'n_intervals'),
-     Input('wvwma-selector', 'value'),
-     Input('sma-selector', 'value'),
-     Input('price-line', 'value'),
-     Input('sma-period', 'value'),
-     Input('price-sma', 'value'),
-     Input('price-max-vol', 'value'),
-     ]
+    Output('btc-chart', 'children'),
+    Output('out-btc', 'children'),
+    Input('refresh', 'n_clicks'),
+    Input('vol-level-slider', 'value'),
+    Input('interval-reload', 'n_intervals'),
+    Input('wvwma-selector', 'value'),
+    Input('sma-selector', 'value'),
+    Input('price-line', 'value'),
+    Input('sma-period', 'value'),
+    Input('price-sma', 'value'),
+    Input('price-max-vol', 'value'),
 )
 def update_chart(n, range_vol_level, nn, wvwma_select, sma_select, price_line, sma_period, price_sma, price_max_vol):
     if cry.df.empty:
-        cry.load(limit=LIMIT)
+        raise dash.exceptions.PreventUpdate
+        # cry.load(limit=LIMIT)
     df = cry.df
     # print(df)
     maxV = cry.maxV
-    range_open = {'min': df['Open'].min(), 'max': df['Open'].max()}
-    vol_level0 = range_vol_level[0]
-    vol_level1 = range_vol_level[1]
+    # range_open = {'min': df['Open'].min(), 'max': df['Open'].max()}
+    vol_level0 = (range_vol_level[0]*maxV)/100
+    vol_level1 = (range_vol_level[1]*maxV)/100
     # Фильтровать по критерию Vol >= уровень
     df['max_vol'] = 0
     df['max_vol'] = df['max_vol'].where(df['Volume'] < vol_level0, 13).where(df['Volume'] < vol_level1, 21)
