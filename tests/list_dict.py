987064df7fd706521249498823d030df8d5ea202
astroill <astroill@gmail.com>
2022-05-28 00:11:19 +0300

    FF-02

---
+++ tests/list_dict.py
@@ -0,0 +1,10 @@
+"""
+Program by AstroiLL
+2022
+"""
+
+if __name__ == '__main__':
+    marks = {i: j for i, j in
+               [(5, '5m'), (15, '15m'), (30, '30m'), (60, '1h'), (120, '2h'), (240, '4h'), (720, '12h')]}
+    print(marks)
+
